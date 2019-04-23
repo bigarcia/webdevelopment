e112e62fa21b9ffd88a331414c56abf025f5e5c0
bigarcia <biigarciam@gmail.com>
2019-04-23 09:01:55 -0300

    alteracao do projeto para web service

---
+++ LocacaoJSP/src/java/br/ufscar/dc/dsw/model/Cliente.java
@@ -0,0 +1,79 @@
+package br.ufscar.dc.dsw.model;
+
+import java.sql.Date;
+
+public class Cliente {
+
+    //private int id_cliente;
+    private String email_cliente;
+    private String senha_cliente;
+    private String cpf_cliente;
+    private String nome_cliente;
+    private String telefone_cliente;
+    private String sexo_cliente;
+    private Date nascimento_cliente;
+
+    /*public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }*/
+
+    public String getEmail_cliente() {
+        return email_cliente;
+    }
+
+    public void setEmail_cliente(String email_cliente) {
+        this.email_cliente = email_cliente;
+    }
+
+    public String getSenha_cliente() {
+        return senha_cliente;
+    }
+
+    public void setSenha_cliente(String senha_cliente) {
+        this.senha_cliente = senha_cliente;
+    }
+
+    public String getCpf_cliente() {
+        return cpf_cliente;
+    }
+
+    public void setCpf_cliente(String cpf_cliente) {
+        this.cpf_cliente = cpf_cliente;
+    }
+
+    public String getNome_cliente() {
+        return nome_cliente;
+    }
+
+    public void setNome_cliente(String nome_cliente) {
+        this.nome_cliente = nome_cliente;
+    }
+
+    public String getTelefone_cliente() {
+        return telefone_cliente;
+    }
+
+    public void setTelefone_cliente(String telefone_cliente) {
+        this.telefone_cliente = telefone_cliente;
+    }
+
+    public String getSexo_cliente() {
+        return sexo_cliente;
+    }
+
+    public void setSexo_cliente(String sexo_cliente) {
+        this.sexo_cliente = sexo_cliente;
+    }
+
+    public Date getNascimento_cliente() {
+        return nascimento_cliente;
+    }
+
+    public void setNascimento_cliente(Date nascimento_cliente) {
+        this.nascimento_cliente = nascimento_cliente;
+    }   
+}
