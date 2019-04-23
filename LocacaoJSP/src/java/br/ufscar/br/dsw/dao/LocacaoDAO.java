6c93d316a582dee131be020f1895a6a93da11fab
bigarcia <biigarciam@gmail.com>
2019-04-23 10:52:26 -0300

    Update LocacaoDAO.java

--- LocacaoJSP/src/java/br/ufscar/br/dsw/dao/LocacaoDAO.java
+++ LocacaoJSP/src/java/br/ufscar/br/dsw/dao/LocacaoDAO.java
@@ -17,7 +17,6 @@ public class LocacaoDAO extends GenericDAO {
     private final static String CRIAR_LOCACAO_SQL = "insert into Locadora" + " (email_locadora, senha_locadora, cnpj_locadora, nome_locadora, cidade_locadora)" + "values(?,?,?,?,?)";
     private final static String LISTAR_LOCACAO_CLIENTE_SQL = "select" + "c.cpf_cliente, l.cpnj_locadora, l.data_dia_locacao" + " from Locacao l inner join Cliente c on c.cpf_cliente = l.cpf_cliente" + "where cpf_cliente = ?";
     private final static String LISTAR_LOCACAO_LOCADORA_SQL = "select" + "ld.cnpj_locadora, l.cpf_cliente, l.data_dia_locacao" + " from Locacao l inner join Locadora ld on ld.cnpj_cliente = l.cnpj_cliente" + "where cnpj_locadora = ?";
-
     public Locacao gravarLocacao(Locacao locacao) throws SQLException {
         try (Connection con = this.getConnection()) {
             PreparedStatement ps = con.prepareStatement(CRIAR_LOCACAO_SQL, Statement.RETURN_GENERATED_KEYS);
@@ -48,6 +47,28 @@ public class LocacaoDAO extends GenericDAO {
                 locacao.setData_dia_locadora(rs.getString("data_dia_locadora"));           
                 cliente.setCpf_cliente(rs.getString("cpf_cliente"));
                 locadora.setCpnj_locadora(rs.getString("cnpj_locadora"));
+                ret.add(locacao);
+            }
+        }
+        return ret;
+    }
+    
+        public List<Locacao> listarTodasLocacoesPorLocadora(String locadora_locacao) throws SQLException {
+        List<Locacao> ret = new ArrayList<>();
+        try (Connection con = this.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(LISTAR_LOCACAO_LOCADORA_SQL);
+            ps.setString(1, locadora_locacao);
+            ps.setString(2, locadora_locacao);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Locacao locacao = new Locacao();
+                Locadora locadora = new Locadora();
+                Cliente cliente = new Cliente();
+                
+                locacao.setId_locacao(rs.getInt("id_locacao"));
+                locacao.setData_dia_locadora(rs.getString("data_dia_locadora"));           
+                locadora.setCpnj_locadora(rs.getString("cnpj_locadora"));
+                cliente.setCpf_cliente(rs.getString("cpf_cliente"));
                 ret.add(locacao);
             }
         }
