549e180fa65336ed80094f0e76276a1a429aa89a
Gabrieli Santos <gabrielisantos17.gs@gmail.com>
2019-05-06 20:31:48 -0300

    atualizando a classe br.ufscar.dc.dsw.dao.GenericDAO

--- src/java/br/ufscar/dc/dsw/dao/GenericDAO.java
+++ src/java/br/ufscar/dc/dsw/dao/GenericDAO.java
@@ -1,5 +1,37 @@
 package br.ufscar.dc.dsw.dao;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class GenericDAO<T> {
+
+    private final EntityManagerFactory emf;
+
+    public GenericDAO() {
+        emf = Persistence.createEntityManagerFactory("JPAPU");
+    }
+
+    protected EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    abstract T get(Long id);
+
+    abstract List<T> getAll();
+
+    abstract void save(T t);
+
+    abstract void update(T t);
+
+    abstract void delete(T t);
+}
+
+/* CODIGO DO TRABALHO 1
+
+package br.ufscar.dc.dsw.dao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -17,4 +49,4 @@ abstract public class GenericDAO {
     protected Connection getConnection() throws SQLException {
         return DriverManager.getConnection("jdbc:derby://localhost:1527/Locadora", "root", "root");
     }
-}
+} */
